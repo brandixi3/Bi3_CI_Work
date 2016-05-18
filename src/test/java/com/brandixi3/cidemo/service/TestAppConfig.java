e4ddcad75227c28dab5beb090eda5aa1f5e30c34
Janith Bandara <janith3000@gmail.com>
2016-05-18 11:48:39 +0530

    added mock test cases

--- src/test/java/com/brandixi3/cidemo/service/TestAppConfig.java
+++ src/test/java/com/brandixi3/cidemo/service/TestAppConfig.java
@@ -1,14 +1,35 @@
 package com.brandixi3.cidemo.service;
 
+import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.brandixi3.cidemo.dao.InventryDao;
 import com.brandixi3.cidemo.service.impl.InventoryServiceImpl;
 
+/**
+ * The Class TestAppConfig.
+ */
 @Configuration
 public class TestAppConfig {
+	
+	/**
+	 * Gets the sample service.
+	 *
+	 * @return the sample service
+	 */
 	@Bean
 	public InventoryService getSampleService() {
 		return new InventoryServiceImpl();
+	}
+
+	/**
+	 * My mock.
+	 *
+	 * @return the inventry dao
+	 */
+	@Bean
+	public InventryDao myMock() {
+		return Mockito.mock(InventryDao.class);
 	}
 }
